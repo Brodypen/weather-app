2c29db3f738a8629013905e96aa86c43e814b6ba
Brody <brodypen7987@gmail.com>
2022-09-04 14:52:02 -0500

    Add API key

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,24 @@
-import * as React from "react"
-import {
-  ChakraProvider,
-  Box,
-  Stack,
-  theme,
-  Heading,
-} from "@chakra-ui/react"
-import { ColorModeSwitcher } from "./components/ColorModeSwitcher"
+import * as React from "react";
+import { ChakraProvider, Box, Stack, theme, Heading } from "@chakra-ui/react";
+import { ColorModeSwitcher } from "./components/ColorModeSwitcher";
+import Search from "./utils/Search";
+export const App = () => {
+  const url =
+    "https://api.openweathermap.org/data/2.5/weather?q=houston&appid=4cb08b58b0f30c6dd7ac0e4c4b487572";
 
-export const App = () => (
-  <ChakraProvider theme={theme}>
-    <Box textAlign="center" fontSize="xl">
-      <Stack minH="100vh" p={3}>
-        <ColorModeSwitcher alignSelf="flex-end" />
-        <Heading>Weather App</Heading>
-      </Stack>
-    </Box>
-  </ChakraProvider>
-)
+
+  return (
+    <ChakraProvider theme={theme}>
+      <Box textAlign="center" fontSize="xl">
+        <Stack minH="100vh" p={3}>
+          {
+            // Here
+          }
+          <ColorModeSwitcher alignSelf="flex-end" />
+          <Search />
+          <Heading>Weather App</Heading>
+        </Stack>
+      </Box>
+    </ChakraProvider>
+  );
+};
---
+++ src/utils/Search.tsx
@@ -0,0 +1,6 @@
+import * as React from "react";
+const Search = () => {
+  return (<>Search</>);
+}
+
+export default Search;
