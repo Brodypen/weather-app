743a3cee450f5c07a2e8a14a1ad7c217c2cbc14f
Brody <brodypen7987@gmail.com>
2022-09-04 11:01:28 -0500

    Basic file layout add

--- src/App.tsx
+++ src/App.tsx
@@ -2,37 +2,19 @@ import * as React from "react"
 import {
   ChakraProvider,
   Box,
-  Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
+  Stack,
   theme,
+  Heading,
 } from "@chakra-ui/react"
-import { ColorModeSwitcher } from "./ColorModeSwitcher"
-import { Logo } from "./Logo"
+import { ColorModeSwitcher } from "./components/ColorModeSwitcher"
 
 export const App = () => (
   <ChakraProvider theme={theme}>
     <Box textAlign="center" fontSize="xl">
-      <Grid minH="100vh" p={3}>
-        <ColorModeSwitcher justifySelf="flex-end" />
-        <VStack spacing={8}>
-          <Logo h="40vmin" pointerEvents="none" />
-          <Text>
-            Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
-          </Text>
-          <Link
-            color="teal.500"
-            href="https://chakra-ui.com"
-            fontSize="2xl"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn Chakra
-          </Link>
-        </VStack>
-      </Grid>
+      <Stack minH="100vh" p={3}>
+        <ColorModeSwitcher alignSelf="flex-end" />
+        <Heading>Weather App</Heading>
+      </Stack>
     </Box>
   </ChakraProvider>
 )
